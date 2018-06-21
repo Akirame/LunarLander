2d71e5052f90c7c30ab2f968a8f2118dd51b121b
Akirame <gsvillalba12@hotmail.com>
2018-06-21 16:47:39 -0300

    Add Zoom

--- Assets/00_Game/Scripts/PlayerController.cs
+++ Assets/00_Game/Scripts/PlayerController.cs
@@ -3,23 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviourSingleton<PlayerController> {
-    
 
+    public delegate void PlayerActions(PlayerController p);
+    public static PlayerActions CloseToGround;
+    public static PlayerActions NotCloseToGround;
+    public LayerMask layers;
     public float verticalForce = 1f;
     public float rotateForce = 1f;
     public float gravityScale = 0.1f;
     public float thresholdWin = 0.05f;    
+    public float zoomDistance;
+
     private Rigidbody2D rig;
+    private bool closer;
+
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         rig.gravityScale = gravityScale;
+        closer = false;       
     }
 
     private void Update()
     {
-        Inputs();        
+        Inputs();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, zoomDistance, layers);
+        if (hit)
+        {
+                CloseToGround(this);
+                closer = true;
+        }
+        else if (closer)
+        {
+            NotCloseToGround(this);
+            closer = false;
+            Debug.Log("HOL");
+
+        }
+        Debug.DrawRay(transform.position, -Vector3.up * zoomDistance);
     }
     private void Inputs()
     {
---
+++ Assets/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviourSingleton<CameraController> {
+
+    private Vector3 startPos;
+    private Camera cam;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        cam = GetComponent<Camera>();
+        
+        PlayerController.CloseToGround += ZoomIn;
+        PlayerController.NotCloseToGround += ZoomOut;
+    }
+
+    public void ZoomIn(PlayerController p)
+    {
+        transform.position = new Vector3(p.transform.position.x,p.transform.position.y,transform.position.z);
+        cam.orthographicSize = 2;
+        Debug.Log("HOLA");
+
+    }
+    public void ZoomOut(PlayerController p)
+    {
+        transform.position = startPos;
+        cam.orthographicSize = 5;
+        Debug.Log("HOLA");
+    }
+}
