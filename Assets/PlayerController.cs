d5d76160ab6d11cc004457f08e07ed3ee7014731
Akirame <gsvillalba12@hotmail.com>
2018-06-21 15:36:48 -0300

    Add PlayerMovement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float verticalForce = 1f;
+    public float rotateForce = 1f;
+    public float gravityScale = 0.1f;
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.gravityScale = gravityScale;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        {
+            rig.AddForce(transform.up * verticalForce, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,transform.eulerAngles.z + rotateForce);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z - rotateForce);
+        }
+    }
+}
