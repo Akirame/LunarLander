1616a64b92ee6042a89d300cdbd10c655c65ba7a
Akirame <gsvillalba12@hotmail.com>
2018-06-22 18:04:59 -0300

    Add Main Menu (Without transition scene)

---
+++ Assets/00_Game/Scripts/UIMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMainMenu : MonoBehaviour {
+
+    public GameObject canvasMenu;
+    public GameObject CanvasCredits;
+
+    private void Start()
+    {
+        canvasMenu.SetActive(true);
+        CanvasCredits.SetActive(false);
+    }
+    public void ActivateMenu()
+    {
+        canvasMenu.SetActive(true);
+        CanvasCredits.SetActive(false);
+    }
+    public void ActivateCredits()
+    {
+        canvasMenu.SetActive(false);
+        CanvasCredits.SetActive(true);
+    }
+    
+}
