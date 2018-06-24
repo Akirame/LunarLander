5ab545fa0862a7d274c35fe4ac86c9a7dccafab6
Akirame <gsvillalba12@hotmail.com>
2018-06-23 21:45:22 -0300

    Add LandZone

---
+++ Assets/00_Game/Scripts/LandZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LandZone : MonoBehaviour {
+
+    public Text mutiplierText;
+    public int multiplier;
+
+    private void Start()
+    {
+        mutiplierText.text = "x" + multiplier;
+    }
+}
